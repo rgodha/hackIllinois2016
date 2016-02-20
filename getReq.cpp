7beffa51c0d3f316e0a2760b16dcdce0e7a02554
rahul <rahul@localhost.localdomain>
2016-02-20 05:40:15 -0500

    Initial Commit

--- getReq.cpp
+++ getReq.cpp
@@ -25,7 +25,6 @@ string url_encode(const string &value)
         escaped << uppercase;
         escaped << '%' << setw(2) << int((unsigned char) c);
         escaped << nouppercase;
-	
     }
 
     return escaped.str();
@@ -33,6 +32,7 @@ string url_encode(const string &value)
 
 int main(int argc, char *argv[])
 {
+	CURLcode curl_res;
 	CURL *curl = curl_easy_init();
 	string client = "firefox";
 	string apikey = "AIzaSyCdA-CmA7dusGVUIw3d9LubMumv-JgqxMg";
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 	string URL = "http://www.google.com";
 	//string URL = "https://sb-ssl.google.com/safebrowsing/api/lookup?client=fedora&key=AIzaSyCdA-CmA7dusGVUIw3d9LubMumv-JgqxMg&appver=1.01&pver=3.1&url="+URLReq;
 	//string URL1 = "https://sb-ssl.google.com/safebrowsing/api/lookup?client=fedora&key=AIzaSyCdA-CmA7dusGVUIw3d9LubMumv-JgqxMg&appver=1.5.2&pver=3.1";
-	
+ 		
 	string baseURL = "https://sb-ssl.google.com/safebrowsing/api/lookup";
 	string arguments = "";
 	arguments += "client=" + url_encode(client)+"&";
@@ -49,13 +49,19 @@ int main(int argc, char *argv[])
 	arguments += "appver=" + url_encode(appver) + "&";
 	arguments += "pver=" + url_encode(pver) + "&";
 	arguments += "url=" + url_encode(URL);
-
+	
 	string url_to_req = baseURL + "?" + arguments;
 	
+	//string url_to_req = "http://www.google.com";	
 	cout << "URL: " << url_to_req << endl;
-  	curl_easy_setopt(curl, CURLOPT_URL, URL);
+  	curl_easy_setopt(curl, CURLOPT_URL, url_to_req);
   	cout << "Now send the request\n";
-	curl_easy_perform(curl);
-
+	curl_res = curl_easy_perform(curl);
+	
+	if(curl_res != CURLE_OK) {
+		cout << "Error: %s\n" << curl_easy_strerror(curl_res);
+	}
+	
+	curl_easy_cleanup(curl);
 	return 0;
 }
